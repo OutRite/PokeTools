896685095f87f89fd3e0c7c71622c10805bea31e
OutRite <60903237+OutRite@users.noreply.github.com>
2020-10-14 20:51:14 -0700

    Update CorruptSave.py

--- GenerationOne/CorruptSave.py
+++ GenerationOne/CorruptSave.py
@@ -22,17 +22,23 @@ with open(filename, 'rb+') as f:
 	print("Reading file {}...".format(filename))
 	savedata = bytearray(f.read())
 	print("Beginning corruption...")
+	# replace most data with random garbage bytes
 	for corrupted_mem_place in range(0, 0x3523-0x2597):
+		# generate byte
 		corrupted_byte = random.randrange(255)
+		# set data at location to byte
 		savedata[corrupted_mem_place+0x2597] = corrupted_byte
 		# print("Byte: {}".format(corrupted_byte))
 		# print("Address: {}".format(hex(corrupted_mem_place)))
 		print("Corrupted {}%".format(round((corrupted_mem_place/0xF8B)*100, 2)))
 	print("Corrupted!")
 	print("Saving...")
+	# ", 0" may not be necessary [we can probably just do f.seek(0). TODO!]
 	f.seek(0, 0)
+	# write out the data
 	f.write(savedata)
 	print("Complete. Fixing checksum...")
 
+# execute the fix_that_checksum script in a really poor way. TODO: make it a library
 os.system('python3 fix_that_checksum.py {}'.format(filename))
 print("gg rip your save")
