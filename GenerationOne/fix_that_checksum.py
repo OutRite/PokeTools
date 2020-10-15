fb517339f548a899c4992ee2b783eed547a5aa47
OutRite <60903237+OutRite@users.noreply.github.com>
2020-10-14 20:53:29 -0700

    Update fix_that_checksum.py

--- GenerationOne/fix_that_checksum.py
+++ GenerationOne/fix_that_checksum.py
@@ -9,15 +9,20 @@ if (len(sys.argv) < 2):
 
 with open(sys.argv[1], 'rb+') as f:
 	print("Reading file {}".format(sys.argv[1]))
+	# read the data into a bytearray for some reason
 	savedata = bytearray(f.read())
 	print("Current checksum: 0xff")
+	# initialize the checksum to 0xff
 	checksum = 0xff
 	for c in savedata[0x2598:0x3523]:
+		# calculate the checksum by going through all data and subtracting the checksum from that data
 		checksum -= c
 		print("Current checksum: {}".format(hex(checksum)))
 	print("Found checksum: {}".format(checksum&0xff))
+	# use an AND to reduce it to one byte, as the Pokemon games take.
 	savedata[0x3523] = checksum & 0xff
 	print("Writing checksum to file...")
 	f.seek(0, 0)
+	# write out the data
 	f.write(savedata)
 	print("Complete!")
